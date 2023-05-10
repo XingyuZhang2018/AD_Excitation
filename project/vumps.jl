66fdfa7ed77842364275421facb98efdb87e5eba
XingyuZhang2018 <zhangxingyu@iphy.ac.cn>
2023-05-10 16:48:34 +0800

    vumps project file

--- project/vumps.jl
+++ project/vumps.jl
@@ -5,42 +5,104 @@ function parse_commandline()
     s = ArgParseSettings()
 
     @add_arg_table! s begin
-        "--config"
-            help = "config file"
+        "--if_json_config"
+            arg_type = Bool
+            default = false
+        "--model"
+            help = "model"
             arg_type = String
-            default = "./project/config.json"
+        "--maxiter"
+            help = "number of iterations"
+            arg_type = Int
+            default = 1000
+        "--tol"
+            help = "tolerance"
+            arg_type = Float64
+            default = 1e-8
+        "--Ni"
+            help = "Ni"
+            arg_type = Int
+            default = 1
+        "--Nj"
+            help = "Nj"
+            arg_type = Int
+            default = 1
+        "--chi"
+            help = "vumps virtual bond dimension"
+            arg_type = Int
+        "--atype"
+            help = "atype"
+            arg_type = String
+        "--infolder"
+            help = "infolder"
+            arg_type = String
+            default = "../data/"
+        "--outfolder"
+            help = "outfolder"
+            arg_type = String
+            default = "../data/"
+        "--verbose"
+            help = "verbose"
+            arg_type = Bool
+            default = true
+        "--if4site"
+            help = "if4site"
+            arg_type = Bool
+            default = false
     end
 
     return parse_args(s)
 end
 
-function main(config_file)
-    config = JSON.parsefile(config_file)  
-    
-    model = eval(Meta.parse(config["model"]))
-
-    Ni = config["mps"]["Ni"]
-    Nj = config["mps"]["Nj"]
-    χ = config["mps"]["χ"]
-    targχ = config["mps"]["targχ"]
-    if4site = config["mps"]["if4site"]
-
-    iters = config["vumps"]["iters"]
-    tol = config["vumps"]["tol"]
-    show_every = config["vumps"]["show_every"]
-    
-    Random.seed!(config["data"]["seed"]) 
-    atype = eval(Meta.parse(config["data"]["atype"])) 
-    infolder = config["data"]["infolder"]
-    outfolder = config["data"]["outfolder"]
+function main()
+    parsed_args = parse_commandline()
+    json_string = JSON.json(parsed_args)
 
     outfolder = joinpath(outfolder, "$model", "groundstate")
     !isdir(outfolder) && mkpath(outfolder)
-    cp(config_file, joinpath(outfolder,"canonical_mps_$(Ni)x$(Nj)_D$(D)_χ$(targχ).json"); force=true)
-    
-    parsed_args = parse_commandline()
-    config_file = parsed_args["config"]
-    @time vumps(config_file)
+    config_file = joinpath(outfolder,"canonical_mps_$(Ni)x$(Nj)_D$(D)_χ$(targχ).json")
+
+    open(config_file, "w") do file
+        write(file, json_string)
+    end
+
+    if_json_config = parsed_args["if_json_config"]
+    if if_json_config
+        config = JSON.parsefile(config_file)
+        model = eval(Meta.parse(config["model"]))
+        maxiter = config["maxiter"]
+        tol = config["tol"]
+        Ni = config["Ni"]
+        Nj = config["Nj"]
+        χ = config["χ"]
+        atype = eval(Meta.parse(config["atype"]))
+        infolder = config["infolder"]
+        outfolder = config["outfolder"]
+        verbose = config["verbose"]
+        if4site = config["if4site"]
+    else
+        model = eval(Meta.parse(parsed_args["model"]))
+        maxiter = parsed_args["maxiter"]
+        tol = parsed_args["tol"]
+        Ni = parsed_args["Ni"]
+        Nj = parsed_args["Nj"]
+        χ = parsed_args["chi"]
+        atype = eval(Meta.parse(parsed_args["atype"]))
+        infolder = parsed_args["infolder"]
+        outfolder = parsed_args["outfolder"]
+        verbose = parsed_args["verbose"]
+        if4site = parsed_args["if4site"]
+    end
+
+    find_groundstate(model, alg(maxiter = maxiter, tol = tol);
+                     Ni = Ni, Nj = Nj,
+                     χ = χ,
+                     atype = atype,
+                     infolder = infolder,
+                     outfolder = outfolder,
+                     verbose = verbose,
+                     if4site = if4site
+                     );
 end
 
 main()
--- project/vumps.sh
+++ project/vumps.sh
@@ -5,4 +5,8 @@
 #SBATCH --time=9999:00:00							#总运行时间，单位小时
 module load julia-1.7.1
 project_dir=~/research/AD_Excitation
-julia --project=${project_dir} ${project_dir}/project/vumps.jl --config ${project_dir}/project/config.json
+julia --project=${project_dir} ${project_dir}/project/vumps.jl \
+      --if_json_config false \
+      --model J1J2(4,0.4) \
+      --chi 1024 \
+      --if4site true
