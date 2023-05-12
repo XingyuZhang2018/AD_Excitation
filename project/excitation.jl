86e85965dd9bfb2305385ba2696c7439551c35bb
xyzhang <xyzhang@bright90.cm.cluster>
2023-05-12 11:52:44 +0800

    excitation project file

--- project/excitation.jl
+++ project/excitation.jl
@@ -1,14 +1,58 @@
 using AD_Excitation
 using ArgParse
+using CUDA
 
 function parse_commandline()
     s = ArgParseSettings()
 
     @add_arg_table! s begin
-        "--config"
-            help = "config file"
+        "--model"
+            help = "model"
             arg_type = String
-            default = "./project/config.json"
+        "--atype"
+            help = "atype"
+            arg_type = String
+        "--kx"
+            arg_type = Float64
+            required = true
+        "--ky"
+            arg_type = Float64
+            required = true
+        "--n"
+            help = "howmany state"
+            arg_type = Int
+            default = 1
+        "--Ni"
+            help = "Ni"
+            arg_type = Int
+            default = 1
+        "--Nj"
+            help = "Nj"
+            arg_type = Int
+            default = 1
+        "--chi"
+            help = "vumps virtual bond dimension"
+            arg_type = Int
+        "--infolder"
+            help = "infolder"
+            arg_type = String
+            default = "../data/"
+        "--outfolder"
+            help = "outfolder"
+            arg_type = String
+            default = "../data/"
+        "--verbose"
+            help = "verbose"
+            arg_type = Bool
+            default = true
+        "--if4site"
+            help = "if4site"
+            arg_type = Bool
+            default = false
+        "--ifmerge"
+            help = "ifmerge"
+            arg_type = Bool
+            default = false
     end
 
     return parse_args(s)
@@ -16,8 +60,28 @@ end
 
 function main()
     parsed_args = parse_commandline()
-    config_file = parsed_args["config"]
-    @time excitation_spectrum_canonical_MPO(config_file)
+
+    model = eval(Meta.parse(parsed_args["model"]))
+    atype = eval(Meta.parse(parsed_args["atype"]))
+    kx = parsed_args["kx"]
+    ky = parsed_args["ky"]
+    n = parsed_args["n"]
+    Ni = parsed_args["Ni"]
+    Nj = parsed_args["Nj"]
+    χ = parsed_args["chi"]
+    infolder = parsed_args["infolder"]
+    outfolder = parsed_args["outfolder"]
+    verbose = parsed_args["verbose"]
+    if4site = parsed_args["if4site"]
+    ifmerge = parsed_args["ifmerge"]
+
+    @time excitation_spectrum_canonical_MPO(model, (kx*2*pi/model.W,ky*2*pi/model.W), n;
+                                            Ni = Ni, Nj = Nj,
+                                            χ = χ,
+                                            atype = atype,
+                                            ifmerge = ifmerge,
+                                            if4site = if4site,
+                                            infolder = infolder, outfolder = outfolder)
 end
 
 main()
--- project/excitation.sh
+++ project/excitation.sh
@@ -5,4 +5,11 @@
 #SBATCH --time=9999:00:00							#总运行时间，单位小时
 module load julia-1.7.1
 project_dir=~/research/AD_Excitation
-julia --project=${project_dir} ${project_dir}/project/excitation.jl --config ${project_dir}/project/config.json
+julia \
+--project=${project_dir} ${project_dir}/project/excitation.jl \
+--atype "CuArray" \
+--model "J1J2(6, 0.5)" \
+--if4site true \
+--chi 256 \
+--n 10 \
+--kx 0 --ky 0
