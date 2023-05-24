ceedf17b1d42b93679a0c790ee3d568978a45b24
xyzhang <xyzhang@bright90.cm.cluster>
2023-05-24 11:02:54 +0800

    [(0,0),(pi,0),(0,pi)] for spectral weight

--- project/spectral_weight_example.jl
+++ project/spectral_weight_example.jl
@@ -1,13 +1,15 @@
 using AD_Excitation
 using CUDA
 
-for W in 4:4, J2 in 0.4:0.1:0.4, x in 0:0, y in 0:0, χ in 2 .^ (9:9)
+for W in 4, J2 in 0.49:0.01:0.49, x in 0:0, y in 0:0, χ in 2 .^ (7:8)
     @show χ
     model = J1J2(W, J2)
-    k = (0*pi/W,0*pi/W)
-    @time spectral_weight(model, k, 10; 
-                          Nj = 1, χ = χ, atype = CuArray, 
-                          infolder = "./data/", outfolder = "./data/", 
-                          ifmerge = false, if4site = true
-                          )
+    # k = (x*pi/W,y*pi/W)
+    for k in [(0,0),(pi,0),(0,pi)]
+        @time spectral_weight(model, k, 20; 
+                              Nj = 1, χ = χ, atype = CuArray, 
+                              infolder = "./data/", outfolder = "./data/", 
+                              ifmerge = false, if4site = true
+                              )
+    end
 end
