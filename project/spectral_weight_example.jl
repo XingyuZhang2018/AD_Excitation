7934a005c43d1c14220971fbf15f700c636f1d96
XingyuZhang2018 <zhangxingyu@iphy.ac.cn>
2023-06-29 22:19:18 +0800

    spectral_weight_dimer

--- project/spectral_weight_example.jl
+++ project/spectral_weight_example.jl
@@ -1,16 +1,17 @@
 using AD_Excitation
 using CUDA
 
-for W in 4, J2 in 0.5:0.01:0.7, x in 0:0, y in 0:0, χ in 2 .^ (8:8)
+for W in 5, J2 in 0.5:0.01:0.5, x in 0:0, y in 0:0, χ in 2 .^ (7:7)
     @show χ
     model = J1J2(W, J2)
     # k = (x*pi/W,y*pi/W)
-    for k in [(0,0),(pi,0),(0,pi),(pi,pi)]
+    for k in [(pi,pi)]
     # for k in [(pi/2,pi/2)]
-        @time spectral_weight(model, k, 30; 
+        @time ωk = spectral_weight_dimer(model, k, 30; 
                               Nj = 1, χ = χ, atype = CuArray, 
-                              infolder = "./data/", outfolder = "./data/", 
+                              infolder = "../data/", outfolder = "../data/", 
                               ifmerge = false, if4site = true
                               )
+        @show ωk
     end
 end
--- src/iMPS/canonical/observable.jl
+++ src/iMPS/canonical/observable.jl
@@ -1,6 +1,6 @@
 export load_canonical_excitaion
 export energy_gs_canonical_MPO
-export spectral_weight, correlation_length, spin_config, S2_total
+export spectral_weight, spectral_weight_dimer, correlation_length, spin_config, S2_total
 
 function load_canonical_excitaion(infolder, model, Nj, D, χ, k)
     kx, ky = k
@@ -59,7 +59,7 @@ end
     ```
 
 """
-function ω(W, k, AC, AL, AR, S, B, ƆLL, CRR)
+function ω(W, k, AC, AL, AR, S, B, ƆLL)
     # 1. AS and B on the same site
     ωk = ein"(abdij,cb),acdij->"(AC,S,conj(B))
 
@@ -72,11 +72,11 @@ end
 
 """
     ```
-     ┌───AL───┬─           ┌────┬──── c
+     ┌───AL───┬─           ┌────┬────c
      │   │    │            │    b    
      │   S    s2           a    │    
      │   │    │            │    d    
-     └───AL*──┴─           └────┴──── e 
+     └───AL*──┴─           └────┴────e 
     ```
 """
 function einCS(W, k, AL, S, Ɔ)
@@ -91,6 +91,149 @@ function einCS(W, k, AL, S, Ɔ)
     return CS
 end
 
+"""
+    ω_m = ω2(W, k, AL, AR, S, Bu)
+
+    get the spectral weight `|<Ψₖ(B)|SₖSₖ|Ψₖ(A)>|²`
+    ```
+    1. S1 and S2 are next to each other and on the same two sites
+        ┌───AC──AR──┐     ┌───AC──AR──┐           ┌───┬─c─┬───┐
+        │   │   │   │     │   │   │   │           │   b   d   │
+        │   S1  S2  │  +  │   S1  S2  │           a   │   │   e
+        │   │   │   │     │   │   │   │           │   f   h   │
+        └───B*──AR──┘     └───AL──B*──┘           └───┴─g─┴───┘
+    
+    2. S1 and S2 next to each other and on the different sites
+        ┌───AL──AL──┬───AC──┐
+        │   │   │   │   │   │
+        │   S1  S2  s2  │   │
+        │   │   │   │   │   │
+        └───AL*─AL*─┴───B*──┘
+
+        s2 = sum of `eⁱᵏ 工` series: 
+                                                                             
+             ───         ─┬─             ─┬─┬─           ─┬─┬─┬─               ─┬─┬─...─┬─ 
+        eⁱ⁰ᵏ      +  eⁱ¹ᵏ │    +    eⁱ²ᵏ  │ │    +  eⁱ³ᵏ  │ │ │   ... +   eⁱⁿᵏ  │ │     │   ...
+             ───         ─┴─             ─┴─┴─           ─┴─┴─┴─               ─┴─┴─...─┴─ 
+
+    ```
+
+"""
+function ω2(W, k, AC, AL, AR, S, B, ƆLL)
+    S1, S2 = S
+    kx, ky = k
+    # 1. S1 and S2 are next to each other and on the same two sites
+    ωk = ein"((((abcij,fb),afgij),cdeij),hd),gheij->"(AC,S1,conj(B),AR,S2,conj(AR)) +
+         ein"((((abcij,fb),afgij),cdeij),hd),gheij->"(AC,S1,conj(AL),AR,S2,conj(B)) * exp(1.0im * ky)
+
+    # S1 and S2 next to each other and on the different sites
+    CSS = einCSS(W, k, AL, S, ƆLL)
+    ωk += ein"(abcij,adij),dbcij->"(AC,CSS,conj(B))
+          
+    return abs2(Array(ωk)[])
+end
+
+"""
+    ```
+     ┌───AL──AL──┬─        ┌───┬─d─┬──f
+     │   │   │   │         │   b   e 
+     │   S1  S2  s2        a   │   │ 
+     │   │   │   │         │   g   k 
+     └───AL*─AL*─┴─        └───┴─h─┴──l 
+    ```
+"""
+function einCSS(W, k, AL, S, Ɔ)
+    kx, ky = k
+    S1, S2 = S
+    C工工  = ein"((((abdij,gb),aghij),defij),ke),hklij->flij"(AL, S1, conj(AL), AL, S2, conj(AL))
+    coef = series_coef_L(k, W)
+    C工工s = sum(collect(Iterators.take(iterated(x->C工map(x, AL, AL), C工工), W)) .* circshift(coef,-1))
+    χ = size(AL,1)
+    C = _arraytype(AL)(reshape(I(χ), (χ,χ,1,1)))
+    CSS, info = linsolve(CSS->CSS - exp(1.0im * kx) * nth(iterated(x->C工map(x, AL, AL), CSS), W+1) + exp(1.0im * kx) * ein"(abij,abij),cdij->cdij"(CSS, Ɔ, C), C工工s)
+    @assert info.converged == 1
+    return CSS
+end
+
+"""
+    ω_m = ω3(W, k, AL, AR, S, Bu)
+
+    get the spectral weight `|<Ψₖ(B)|SₖSₖ|Ψₖ(A)>|²`
+    ```
+    1. S1 and S2 keep away from W site next to each other and on the different sites
+
+        ┌───AC─...─AR───┐
+        │   │      │    │
+        │   S1 ... S2   │   (W terms)
+        │   │      │    │
+        └───B*─...─AR*──┘
+
+    2. S1 and S2 next to each other and on the different sites
+
+        ┌───AL──...─AL──┬───AC──┐
+        │   │       │   │   │   │
+        │   S1  ... S2  s2  │   │
+        │   │       │   │   │   │
+        └───AL*─...─AL*─┴───B*──┘    
+
+        s2 = sum of `eⁱᵏ 工` series: 
+                                                                             
+             ───         ─┬─             ─┬─┬─           ─┬─┬─┬─               ─┬─┬─...─┬─ 
+        eⁱ⁰ᵏ      +  eⁱ¹ᵏ │    +    eⁱ²ᵏ  │ │    +  eⁱ³ᵏ  │ │ │   ... +   eⁱⁿᵏ  │ │     │   ...
+             ───         ─┴─             ─┴─┴─           ─┴─┴─┴─               ─┴─┴─...─┴─ 
+
+    ```
+
+"""
+function ω3(W, k, AC, AL, AR, S, B, ƆLL)
+    S1, S2 = S
+    coef = series_coef_L(k, W)
+
+    # 1. S1 and S2 keep away from W site next to each other and on the different sites
+    ωk_r = ein"(abcij,db),edcij->aeij"(AR,S2,conj(AR))
+    ωk_r = nth(iterated(x->工Ɔmap(x, AR, AR), ωk_r), W-1)
+    ωk = ein"((abcij,db),adeij),ceij->"(AC,S1,conj(B),ωk_r)
+    for i in 2:W-1
+        ωk_l = ein"(abcij,db),adeij->ceij"(AL,S1,conj(AL))
+        ωk_l = nth(iterated(x->C工map(x, AL, AL), ωk_l), i-1)
+        ωk_r = ein"(abcij,db),edcij->aeij"(AR,S2,conj(AR))
+        ωk_r = nth(iterated(x->工Ɔmap(x, AR, AR), ωk_r), W-i)
+        ωk += ein"((adij,abcij),dbeij),ceij->"(ωk_l,AC,B,ωk_r) * coef[i-1]
+    end
+    ωk_l = ein"(abcij,db),adeij->ceij"(AL,S1,conj(AL))
+    ωk_l = nth(iterated(x->C工map(x, AL, AL), ωk_l), W-1)
+    ωk += ein"((aeij,abcij),db),edcij->"(ωk_l,AC,S2,conj(B)) * coef[W-1]
+
+    # 2. S1 and S2 next to each other and on the different sites
+    CSAS = einCSAS(W, k, AL, S, ƆLL)
+    ωk += ein"(abcij,adij),dbcij->"(AC,CSAS,conj(B))
+          
+    return abs2(Array(ωk)[])
+end
+
+"""
+    ```
+     ┌───AL──...─AL──┬──         a────┬────c
+     │   │       │   │           │    b    
+     │   S1  ... S2  s2          │    │     
+     │   │       │   │           │    e      
+     └───AL*─...─AL*─┴──         d────┴────f  
+    ```
+"""
+function einCSAS(W, k, AL, S, Ɔ)
+    kx, ky = k
+    S1, S2 = S
+    C工 = ein"(abcij,db),adeij->ceij"(AL,S1,conj(AL))
+    C工 = nth(iterated(x->C工map(x, AL, AL), C工), W-1)
+    C工  = ein"((adij,abcij),eb),defij->cfij"(C工,AL,S2,conj(AL))
+    coef = series_coef_L(k, W)
+    C工s = sum(collect(Iterators.take(iterated(x->C工map(x, AL, AL), C工), W)) .* coef) * exp(1.0im * kx)
+    χ = size(AL,1)
+    C = _arraytype(AL)(reshape(I(χ), (χ,χ,1,1)))
+    CSAS, info = linsolve(CSAS->CSAS - exp(1.0im * kx) * nth(iterated(x->C工map(x, AL, AL), CSAS), W+1) + exp(1.0im * kx) * ein"(abij,abij),cdij->cdij"(CSAS, Ɔ, C), C工s)
+    @assert info.converged == 1
+    return CSAS
+end
 """
 ```
     │  │ 
@@ -129,7 +272,6 @@ function spectral_weight(model, k, m; Nj, χ, infolder, outfolder, atype, ifmerg
         C = reshape(C[:,:,1,2], (χ, χ, 1, 1))
     end
 
-    _, CRR = env_c(AR, conj(AR))
     _, ƆLL = env_ɔ(AL, conj(AL))
 
     AC = ALCtoAC(AL, C)
@@ -149,7 +291,7 @@ function spectral_weight(model, k, m; Nj, χ, infolder, outfolder, atype, ifmerg
     ωk = zeros(Float64, m, 3)
     for i in 1:m
         B = ein"abcij,cdij->abdij"(VL, X[i])
-        ωk[i, :] = [ω(W, k_config, AC, AL, AR, S, B, ƆLL, CRR) for S in S_4s]
+        ωk[i, :] = [ω(W, k_config, AC, AL, AR, S, B, ƆLL) for S in S_4s]
     end
     filepath = joinpath(outfolder, "$model", "canonical/Nj$(Nj)_D$(D2)_χ$(χ)/")
     if4site && (W *= 2)
@@ -157,6 +299,102 @@ function spectral_weight(model, k, m; Nj, χ, infolder, outfolder, atype, ifmerg
     write(logfile, "$(ωk)")
     close(logfile)
     println("save spectral weight to $logfile")
+
+    return ωk
+end
+
+"""
+```
+    │  │ 
+   ─3──4─
+    │  │  
+   ─1──2─
+    │  │ 
+```
+"""
+function SS_4site(model, k)
+    S = model.S
+    kx, ky = k
+    Sα = const_Sx(S), const_Sy(S), const_Sz(S)
+    d = Int(2*S + 1)
+    Id = I(d)
+    [(exp(1.0im * kx) * contract4([S,S,Id,Id]) + exp(1.0im * ky) * contract4([S,Id,S,Id]) + exp(2.0im * kx + 1.0im * ky) *contract4([Id,S,Id,S]) + exp(1.0im * kx + 2.0im * ky) * contract4([Id,Id,S,S]))/4 for S in Sα]
+end
+
+function SS_4site_v(model, k)
+    S = model.S
+    kx, ky = k
+    Sα = const_Sx(S), const_Sy(S), const_Sz(S)
+    d = Int(2*S + 1)
+    Id = I(d)
+    [[exp(1.0im * ky) * contract4([Id,Id,S,Id])/2, exp(2.0im * ky) * contract4([S,Id,Id,Id])/2] for S in Sα], 
+    [[exp(1.0im * kx + 1.0im * ky) * contract4([Id,Id,Id,S])/2, exp(1.0im * kx + 2.0im * ky) * contract4([Id,S,Id,Id])/2] for S in Sα]
+end
+
+function SS_4site_h(model, k)
+    S = model.S
+    kx, ky = k
+    Sα = const_Sx(S), const_Sy(S), const_Sz(S)
+    d = Int(2*S + 1)
+    Id = I(d)
+    [[exp(1.0im * kx) * contract4([Id,S,Id,Id])/2, exp(2.0im * kx) * contract4([S,Id,Id,Id])/2] for S in Sα], 
+    [[exp(1.0im * kx + 1.0im * ky) * contract4([Id,Id,Id,S])/2, exp(2.0im * kx + 1.0im * ky) * contract4([Id,Id,S,Id])/2] for S in Sα]
+end
+
+"""
+
+    get the dimer spectral weight `|<Ψₖ(B)|SₖSₖ|Ψ(A)>|²`
+"""
+function spectral_weight_dimer(model, k, m; Nj, χ, infolder, outfolder, atype, ifmerge, if4site)
+    Mo = if4site ? atype(MPO_2x2(model)) : atype(MPO(model))
+    D2 = size(Mo, 2)
+
+    AL, C, AR = init_canonical_mps(;infolder = joinpath(infolder, "$model", "groundstate"), 
+                                    atype = atype,  
+                                    Nj = Nj,      
+                                    D = D2, 
+                                    χ = χ)
+
+    if ifmerge
+        AL = reshape(ein"abc,cde->abde"(AL[:,:,:,1,1], AL[:,:,:,1,2]), (χ, D2^2, χ, 1, 1))
+        AR = reshape(ein"abc,cde->abde"(AR[:,:,:,1,1], AR[:,:,:,1,2]), (χ, D2^2, χ, 1, 1))
+        C = reshape(C[:,:,1,2], (χ, χ, 1, 1))
+    end
+
+    _, ƆLL = env_ɔ(AL, conj(AL))
+
+    AC = ALCtoAC(AL, C)
+    S_4s = atype.(SS_4site(model, k))
+    S_4v1s, S_4v2s = SS_4site_v(model, k)
+    # S_4h1s, S_4h2s = SS_4site_h(model, k)
+
+    kx, ky = k
+    W = model.W
+    k_config = Float64.([kx, ky])
+    if if4site
+        k_config[1] > pi/2 && (k_config[1] = pi-k_config[1])
+        k_config[2] > pi/2 && (k_config[2] = pi-k_config[2])
+        k_config *= 2
+    end
+    VL, X = load_canonical_excitaion(infolder, model, Nj, D2, χ, k_config)
+    VL = atype(VL)
+    norm(ein"abcij,abdij->cdij"(VL, conj(AL))) < 1e-10 || error("VL and AL are not orthogonal")
+    X  = atype.(X)
+    ωk = zeros(Float64, m, 3)
+    for i in 1:m
+        B = ein"abcij,cdij->abdij"(VL, X[i])
+        ωk[i, :] = [ω(W, k_config, AC, AL, AR, S, B, ƆLL) for S in S_4s] +
+                   ([ω2(W, k_config, AC, AL, AR, atype.(S), B, ƆLL) for S in S_4v1s] + 
+                   [ω2(W, k_config, AC, AL, AR, atype.(S), B, ƆLL) for S in S_4v2s])
+                #    [ω3(W, k_config, AC, AL, AR, atype.(S), B, ƆLL) for S in S_4h1s] + 
+                #    [ω3(W, k_config, AC, AL, AR, atype.(S), B, ƆLL) for S in S_4h2s]  ## discard the horizontal part because it's too small for small 2D correlation length
+    end
+    filepath = joinpath(outfolder, "$model", "canonical/Nj$(Nj)_D$(D2)_χ$(χ)/")
+    if4site && (W *= 2)
+    logfile = open("$filepath/spectral_weight_dimer_kx$(round(Int,kx/pi*W/2))_ky$(round(Int,ky/pi*W/2)).log", "w")
+    write(logfile, "$(ωk)")
+    close(logfile)
+    println("save dimer spectral weight to $logfile")
 
     return ωk
 end
